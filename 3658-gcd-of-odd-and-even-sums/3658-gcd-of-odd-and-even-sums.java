a045256c1d54a29ac075f08b4b47174c3f305089
Rahul <yadavpoly30012005@gmail.com>
2026-07-16 22:04:33 +0530

    Refactor gcdOfOddEvenSums method implementation
    Removed the gcd method and simplified gcdOfOddEvenSums to return n.

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,19 +1,5 @@
-class Solution {
-    // Euclidean algorithm to find the greatest common divisor
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-    
-    public int gcdOfOddEvenSums(int n) {
-        // The sum of the first n odd numbers is always n^2
-        int sumOdd = n * n;
-        
-        // The sum of the first n even numbers is always n * (n + 1)
-        int sumEven = n * (n + 1);
-        
-        return gcd(sumOdd, sumEven);
+class Solution {  
+    public int gcdOfOddEvenSums(int n) {       
+        return n;
     }
 }
