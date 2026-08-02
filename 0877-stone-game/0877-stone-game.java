a2afe3d14be9289d78c24cbe6bbfc2a78fef4db5
Rahul <yadavpoly30012005@gmail.com>
2026-08-02 23:22:41 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.84%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // Alice can always force a win by choosing either 
+        // all odd-indexed piles or all even-indexed piles.
+        return true;
+    }
+}
