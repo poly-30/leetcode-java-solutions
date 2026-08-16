732df4ce3df36580742d62d2990d2bb75df6341c
Rahul <yadavpoly30012005@gmail.com>
2026-08-16 23:33:53 +0530

    Time: 4 ms (75.68%), Space: 114.7 MB (82.43%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
+        for (int stone : stones) {
+            cnt[stone % 3]++;
+        }
+        
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] >= 1 && cnt[2] >= 1;
+        } else {
+            return Math.abs(cnt[1] - cnt[2]) > 2;
+        }
+    }
+}
