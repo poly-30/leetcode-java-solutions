301a3378b8aab32b3970d5f8c98aab2d8eba106f
Rahul <yadavpoly30012005@gmail.com>
2026-08-17 21:35:49 +0530

    Time: 343 ms (19.23%), Space: 47.6 MB (60.58%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
+        }
+
+        int[][] dp = new int[n][n];
+
+        // len is the length of the current interval being solved
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                for (int k = i; k < j; k++) {
+                    int leftSum = prefix[k + 1] - prefix[i];
+                    int rightSum = prefix[j + 1] - prefix[k + 1];
+
+                    if (leftSum < rightSum) {
+                        dp[i][j] = Math.max(dp[i][j], leftSum + dp[i][k]);
+                    } else if (leftSum > rightSum) {
+                        dp[i][j] = Math.max(dp[i][j], rightSum + dp[k + 1][j]);
+                    } else { // leftSum == rightSum
+                        int optionLeft = leftSum + dp[i][k];
+                        int optionRight = rightSum + dp[k + 1][j];
+                        dp[i][j] = Math.max(dp[i][j], Math.max(optionLeft, optionRight));
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
