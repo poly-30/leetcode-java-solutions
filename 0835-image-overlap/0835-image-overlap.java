55ae1896c8dd6578198f16381ffc916d9729e6b0
Rahul <yadavpoly30012005@gmail.com>
2026-09-13 21:42:52 +0530

    Time: 153 ms (27.27%), Space: 47.8 MB (10.98%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        List<int[]> list1 = new ArrayList<>();
+        List<int[]> list2 = new ArrayList<>();
+
+        // Collect coordinates of 1s in both matrices
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (img1[r][c] == 1) list1.add(new int[]{r, c});
+                if (img2[r][c] == 1) list2.add(new int[]{r, c});
+            }
+        }
+
+        // Count frequencies of shift vectors (dx, dy)
+        Map<String, Integer> countMap = new HashMap<>();
+        int maxOverlap = 0;
+
+        for (int[] p1 : list1) {
+            for (int[] p2 : list2) {
+                int dr = p2[0] - p1[0];
+                int dc = p2[1] - p1[1];
+                String key = dr + "," + dc;
+
+                int count = countMap.getOrDefault(key, 0) + 1;
+                countMap.put(key, count);
+                maxOverlap = Math.max(maxOverlap, count);
+            }
+        }
+
+        return maxOverlap;
+    }
+}
